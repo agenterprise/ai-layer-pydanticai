74ca052c9728596f918e37858edc4e91e0564174
michael from semaverse dev <mail@semaverse.com>
2025-09-29 17:53:02 +0200

    return types

--- llmlayer/{{cookiecutter.project_name}}/app/gen/aimodel/{{cookiecutter.llm.uid | aiurnpath}}/model.py
+++ llmlayer/{{cookiecutter.project_name}}/app/gen/aimodel/{{cookiecutter.llm.uid | aiurnpath}}/model.py
@@ -2,7 +2,7 @@ from typing import Dict, Optional
 
 from pydantic import BaseModel, ConfigDict
 from pydantic_ai.models.openai import OpenAIChatModel, AsyncOpenAI
-from pydantic_ai.models import Model
+from pydantic_ai.models import Model, Provider, ModelSettings
 from pydantic_ai.providers.azure import AzureProvider
 from pydantic.dataclasses import dataclass
 from app.gen.config.llm_settings import LLMSetting
@@ -18,14 +18,14 @@ class BaseLanguageModel(AbstractLanguageModel):
     {% endfor %}
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
-    async def get_model(self):Model
+    async def get_model(self) -> Model:
          return OpenAIChatModel(
                 self.settings.{{cookiecutter.llm.uid | aiurnvar}}_model_name,
                 provider = await self.get_provider(),
                 settings = await self.get_setting()
             )
     
-    async def get_provider(self):Provider
+    async def get_provider(self) -> Provider:
         {% if cookiecutter.llm.provider == "aiurn:provider:azure" %}
             return AzureProvider(
                 azure_endpoint=settings.{{cookiecutter.llm.uid | aiurnvar}}_endpoint,
@@ -34,16 +34,7 @@ class BaseLanguageModel(AbstractLanguageModel):
                 )
         {% endif %}
     
-    async def get_provider(self):Provider
-        {% if cookiecutter.llm.provider == "aiurn:provider:azure" %}
-            return AzureProvider(
-                azure_endpoint=settings.{{cookiecutter.llm.uid | aiurnvar}}_endpoint,
-                api_version=settings.{{cookiecutter.llm.uid | aiurnvar}}_version,
-                api_key=settings.{{cookiecutter.llm.uid | aiurnvar}}_api_key
-                )
-        {% endif %}
-    
-    async def get_setting(self):
+    async def get_setting(self) -> ModelSettings:
          return ModelSettings(temperature=0.7, max_tokens=1000)
 
 {{cookiecutter.llm.uid | aiurnvar }} = OpenAIChatModel(
