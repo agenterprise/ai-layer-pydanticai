93876f3ab4d9dab2c9db63f588295c13f460a205
michael from semaverse dev <mail@semaverse.com>
2025-09-29 13:07:39 +0200

    tool --> agent

--- agentlayer/{{cookiecutter.project_name}}/app/ext/agents/{{cookiecutter.agent.uid | aiurnpath}}/.example.py
+++ agentlayer/{{cookiecutter.project_name}}/app/ext/agents/{{cookiecutter.agent.uid | aiurnpath}}/.example.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from pydantic_ai import Agent
-from app.gen.agents.{{cookiecutter.tool.uid | aiurnimport}}.tool.agent import BaseAgent
+from app.gen.agents.{{cookiecutter.agent.uid | aiurnimport}}.tool.agent import BaseAgent
 class CustomAgent(BaseAgent):
    
     systemprompt: str = f"Hi Michael" 
