bc6a90ed61165e2af0a1570db759c7f45b6099fb
michael from semaverse dev <mail@semaverse.com>
2025-10-06 18:12:30 +0200

    rm quotes

--- agentlayer/{{cookiecutter.project_name}}/app/gen/agents/{{cookiecutter.agent.uid | aiurnpath}}/response.py
+++ agentlayer/{{cookiecutter.project_name}}/app/gen/agents/{{cookiecutter.agent.uid | aiurnpath}}/response.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel, Field
 
 class {{cookiecutter.agent.uid | aiurnvar | capitalize }}AgentResponse(BaseModel):
     {% for key, value in cookiecutter.agent.outputproperties.items() %}
-    {{ key | aiurnvar }}: str = Field("",description="{{value['description']}}")
+    {{ key | aiurnvar }}: str = Field("",description="{{value['description'] | replace('"','')}}")
     {% endfor %}
    
