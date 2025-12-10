6870502088b8d5f64ed759218a6a31d8b70ecfb6
michael from semaverse dev <mail@semaverse.com>
2025-12-10 21:17:02 +0100

    settings in layer

---
+++ agentlayer/{{cookiecutter.project_name}}/app/gen/config/base_llm.py
@@ -0,0 +1,22 @@
+from app.gen.middleware.AgentBroker import AgentBroker
+from pydantic_settings import BaseSettings
+
+
+class LLMSettings(BaseSettings):
+    {%- for key, llm in cookiecutter.llms.items() %}
+    {{llm.uid | aiurnvar}}_api_key:str = "your api key"
+    {{llm.uid | aiurnvar}}_model_name:str = Field({{llm.model}}, description="Model name or identifier")
+    {{llm.uid | aiurnvar}}_endpoint:str = Field({{llm.endpoint}}, description="API endpoint for the LLM")
+    {{llm.uid | aiurnvar}}_version:str = Field({{llm.version}}, description="Version of the LLM API")
+    {%- endfor %}
+
+
+class BaseLLMEnvironmentContext():
+
+     """ Language Models""" 
+    {%- for key, llm in cookiecutter.llms.items() %}
+    def {{llm.uid | aiurnvar | capitalize }}LLMBean(self):
+        from app.gen.aimodel.{{llm.uid | aiurnimport}}.model import BaseLanguageModel as {{llm.uid | aiurnvar | capitalize}}
+        return {{llm.uid | aiurnvar | capitalize}}(settings=LLMSettingsBean())
+    {%- endfor %}
+
