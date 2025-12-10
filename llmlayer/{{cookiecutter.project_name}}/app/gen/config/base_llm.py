dc05f24799ce2d9859b7aedb8f985cb02418ec2c
michael from semaverse dev <mail@semaverse.com>
2025-12-10 21:38:42 +0100

    setting solo

--- llmlayer/{{cookiecutter.project_name}}/app/gen/config/base_llm.py
+++ llmlayer/{{cookiecutter.project_name}}/app/gen/config/base_llm.py
@@ -1,16 +1,4 @@
-from pydantic import Field
-from app.gen.middleware.AgentBroker import AgentBroker
-from pydantic_settings import BaseSettings
-
-
-class LLMSettings(BaseSettings):
-    {%- for key, llm in cookiecutter.llms.items() %}
-    {{llm.uid | aiurnvar}}_api_key:str = "your api key"
-    {{llm.uid | aiurnvar}}_model_name:str = Field({{llm.model}}, description="Model name or identifier")
-    {{llm.uid | aiurnvar}}_endpoint:str = Field({{llm.endpoint}}, description="API endpoint for the LLM")
-    {{llm.uid | aiurnvar}}_version:str = Field({{llm.version}}, description="Version of the LLM API")
-    {%- endfor %}
-
+from app.gen.config.settings_llm import LLMSettings
 
 class BaseLLMEnvironmentContext():
 
@@ -21,6 +9,6 @@ class BaseLLMEnvironmentContext():
     {%- for key, llm in cookiecutter.llms.items() %}
     def {{llm.uid | aiurnvar | capitalize }}LLMBean(self):
         from app.gen.aimodel.{{llm.uid | aiurnimport}}.model import BaseLanguageModel as {{llm.uid | aiurnvar | capitalize}}
-        return {{llm.uid | aiurnvar | capitalize}}(settings=LLMSettingsBean())
+        return {{llm.uid | aiurnvar | capitalize}}(settings=self.LLMSettingsBean())
     {%- endfor %}
 
