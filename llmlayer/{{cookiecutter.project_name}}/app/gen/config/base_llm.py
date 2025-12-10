1358e6d55d7811fe9fe94eaab13032bf5cab67fa
michael from semaverse dev <mail@semaverse.com>
2025-12-10 21:39:44 +0100

    indenation

--- llmlayer/{{cookiecutter.project_name}}/app/gen/config/base_llm.py
+++ llmlayer/{{cookiecutter.project_name}}/app/gen/config/base_llm.py
@@ -5,7 +5,7 @@ class BaseLLMEnvironmentContext():
     def LLMSettingsBean(self):
         return LLMSettings()
     
-     """ Language Models""" 
+    """ Language Models""" 
     {%- for key, llm in cookiecutter.llms.items() %}
     def {{llm.uid | aiurnvar | capitalize }}LLMBean(self):
         from app.gen.aimodel.{{llm.uid | aiurnimport}}.model import BaseLanguageModel as {{llm.uid | aiurnvar | capitalize}}
